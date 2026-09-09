17b376919eeb8ad92eb22c89d8f8a4db20e2c4e1
Benanasas <benediktas06@gmail.com>
2026-09-09 14:53:39 +0300

    Sveiki cia mygtukas kuris pakeicia teksto spalva

--- app/src/main/java/com/example/mipt1/MainActivity.kt
+++ app/src/main/java/com/example/mipt1/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.graphics.toColorInt
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,11 +22,15 @@ class MainActivity : AppCompatActivity() {
 
 
         val paspauskButton = findViewById<Button>(R.id.paspauskButton)
+        val spalvaButton = findViewById<Button>(R.id.spalvaButton)
         val patarle = findViewById<TextView>(R.id.patarle)
 
         paspauskButton.setOnClickListener {
             patarle.text = getString(R.string.patarle_kregzde)
-
+        }
+        spalvaButton.setOnClickListener {
+            //naudojau pradzioje .setTextColor taciau anrdoid studio pasiule pakeisti i .toColorInt
+            patarle.setTextColor("#B47ECF".toColorInt())
         }
     }
 }
